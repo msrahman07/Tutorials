304fc922007dbe340c2c988bef32108f02e626ed
msrahman <msrahman@sfu.ca>
2020-07-10 22:38:42 -0700

    added search by genre

--- AspDotNetCore/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ AspDotNetCore/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -20,14 +20,34 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
-                         select m;
-            if(!String.IsNullOrEmpty(searchString)){
+                        select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
-            return View(await movies.ToListAsync());
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
@@ -59,7 +79,7 @@ namespace MvcMovie.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseData,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseData,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -91,7 +111,7 @@ namespace MvcMovie.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseData,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseData,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
--- AspDotNetCore/MvcMovie/MvcMovie/Models/SeedData.cs
+++ AspDotNetCore/MvcMovie/MvcMovie/Models/SeedData.cs
@@ -26,7 +26,8 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseData = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
 
                     new Movie
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseData = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -42,6 +44,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseData = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -50,6 +53,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseData = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
