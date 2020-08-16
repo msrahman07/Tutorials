5b7d92845c0230268ec610f20c1ad6dde34e7877
msrahman <msrahman@sfu.ca>
2020-08-15 21:33:13 -0700

    add map component

--- emberjs/super-rentals/config/environment.js
+++ emberjs/super-rentals/config/environment.js
@@ -46,6 +46,6 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
-
+  ENV.MAPBOX_ACCESS_TOKEN = 'paste your Mapbox access token here';
   return ENV;
 };
